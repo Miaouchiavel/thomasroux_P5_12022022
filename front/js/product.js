95dc7e1ff6713aa10b337ec12ff55f6b68a5b5ba
ROUX <thomas.roux94@hotmail.fr>
2022-03-14 22:55:19 +0100

    prdocut if condition

--- front/js/product.js
+++ front/js/product.js
@@ -2,17 +2,19 @@
 const idProduct = new URL(window.location.href).searchParams.get("id");
 console.log(idProduct);
 
+// crréation de l'image produit 
+let productImg = document.createElement("img");
+
 getProduct();
 // récuperer l'API
 function getProduct() {
     fetch("http://localhost:3000/api/products/" + idProduct)
-        // reponse de l'api sou JSON
+        // reponse de l'api sous JSON
         .then(response => response.json())
         // dispatch de la promesse product dans le DOM 
         .then(product => {
             console.table(product)
                 // Insertion de l'image
-            let productImg = document.createElement("img");
             document.querySelector(".item__img").appendChild(productImg);
             productImg.src = product.imageUrl;
             productImg.alt = product.altTxt;
@@ -32,9 +34,88 @@ function getProduct() {
                 color.setAttribute("value", product.colors[i]);
                 color.innerHTML = product.colors[i];
                 colorsProduct.appendChild(color);
-                console.log(color)
+
 
             }
 
         })
+}
+
+// selection du boutton dans le dom 
+
+const envoyerPanier = document.querySelector("#addToCart");
+
+// écouter le bouton et envoyer le panier 
+
+envoyerPanier.addEventListener("click", (event => {
+
+    //récuperer les valeurs du produit
+    let produitData = {
+        idProduit: idProduct,
+        nameProduit: document.querySelector("#title").textContent,
+        imgProduit: productImg.src,
+        altImg: productImg.alt,
+        quantity: parseInt(document.querySelector("#quantity").value),
+        colorChoice: document.querySelector("#colors").value,
+        prixProduit: document.querySelector("#price").textContent,
+
+    }
+
+
+
+    if (produitData.quantity > 0 && produitData.colorChoice) {
+        addCartProduct(produitData)
+    } else {
+        alert("merci de choisir ")
+    }
+}))
+
+function addCartProduct(produitData) {
+    //Initialisation du local storage Json.parse convertie les données en objet JS
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let localCart = []
+    if (cart) {
+        console.log(localCart)
+        cart.forEach(el => {
+            if (el.idProduit == produitData.idProduit && el.colorChoice == produitData.colorChoice) {
+                el.quantity += produitData.quantity
+                localCart.push(el)
+                console.log("oui")
+            } else {
+                localCart.push(produitData);
+            }
+        })
+        console.log(localCart);
+        localCart.push(produitData);
+        localStorage.setItem("cart", JSON.stringify(localCart));
+
+
+
+    } else {
+        localCart.push(produitData);
+        localStorage.setItem("cart", JSON.stringify(localCart))
+        console.log(localCart)
+
+    }
+    // console.log(cart);
+
+    // let produitLocalStorage = JSON.parse(localStorage.getItem("cart"));
+    // console.log(produitLocalStorage)
+
+    // // si il y a déjà des produits dans local storage 
+    // if (produitLocalStorage) {
+    //     // ajouter condition if idProduct === idProduct && color choice ==== color choice then quantity ++ quantity  else ce qui a écrit en dessous new array 
+
+    //     produitLocalStorage.push(produitData);
+    //     localStorage.setItem("cart", JSON.stringify(produitLocalStorage))
+
+    // }
+    // // si il n'y a  aucun produit dans le local storage
+    // else {
+    //     produitLocalStorage = []
+    //     produitLocalStorage.push(produitData);
+    //     // ajoute au local storage le produit sous form JSON avec le stringify
+
+    // }
+
 }
